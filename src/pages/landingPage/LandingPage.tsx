33f050f2cdb8eb4b666c365ddaac0e503d59a85a
PLThabangR <rakgoropothabang@gmail.com>
2025-08-12 14:46:17 +0200

    basic structure

---
+++ src/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className='landing-page'>LandingPage</div>
+       <p className="lead">
+  This is a lead paragraph. It stands out from regular paragraphs.
+</p>
+
+    </>
+  )
+}
+
+export default LandingPage
