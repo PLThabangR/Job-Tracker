e1f5ca9ff531913e31c566946fbf92e3b9beca31
PLThabangR <rakgoropothabang@gmail.com>
2025-08-19 20:26:56 +0200

    Adding hover to nav text

--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
    <div  className='navbar-style'>
      <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <Link className='router-links' to={'/home'}><h1 className="navbar-brand main-header" >Job Tracker</h1></Link>
+    <Link className='router-links' to={'/home'}><h1 className="navbar-brand main-header post-job" >Job Tracker</h1></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -23,7 +23,7 @@ const Navbar = () => {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         
         <li className="nav-item" >
-          <Link className='router-links' to={'/create-jobs'}><h1 className="nav-link" >Post a job</h1></Link>
+          <Link className='router-links' to={'/create-jobs'}><h1 className="nav-link post-job" >Post a job</h1></Link>
         </li>
        
        
